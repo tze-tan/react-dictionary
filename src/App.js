c8c77e6a0657f13ad412b4ebcb1dc81acfc65e80
tze-tan <59767908+tze-tan@users.noreply.github.com>
2024-02-23 18:38:02 +0100

    fixing some import errors and adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <a href="" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
 }
 
 export default App;
